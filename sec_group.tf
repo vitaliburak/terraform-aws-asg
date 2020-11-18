a69ca2e1116605d977684c552c8600a1799cc581
Vitalii Burak <vvitaliy972@gmail.com>
2020-11-18 01:43:21 +0000

    added readme

--- sec_group.tf
+++ sec_group.tf
@@ -9,6 +9,13 @@ resource "aws_security_group" "asg-sec-group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
